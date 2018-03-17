09ac59493e8cb84c872b8670771a5a7cc6e71b5d
fr0stnk <nik7966@icloud.com>
2018-03-17 20:21:29 +0300

    FirstCommit
    StartCommit

---
+++ LabAutomats/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Threading;
+
+namespace LabAutomats
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool[,] boolArray = new bool[12, 12];
+            string[,] stringArray = new string[12, 12];
+            int coordX = 10;
+            int coordY = 1;
+            int steps = 1;
+
+            // Препятствия лабиринта
+            // Первая строчка
+            boolArray[1, 8] = true;
+            // Вторая строчка
+            boolArray[2, 2] = true;
+            boolArray[2, 8] = true;
+            // Третья строчка
+            boolArray[3, 2] = true;
+            boolArray[3, 3] = true;
+            boolArray[3, 5] = true;
+            boolArray[3, 6] = true;
+            // Четвертая строчка
+            boolArray[4, 3] = true;
+            boolArray[4, 10] = true;
+            // Пятая строчка
+            boolArray[5, 6] = true;
+            boolArray[5, 7] = true;
+            boolArray[5, 8] = true;
+            boolArray[5, 9] = true;
+            boolArray[5, 10] = true;
+            // Шестая строчка
+            boolArray[6, 2] = true;
+            // Седьмая строчка
+            boolArray[7, 5] = true;
+            boolArray[7, 7] = true;
+            boolArray[7, 8] = true;
+            // Восьмая строчка
+            boolArray[8, 4] = true;
+            boolArray[8, 5] = true;
+            boolArray[8, 7] = true;
+            // Девятая строчка
+            boolArray[9, 1] = true;
+            boolArray[9, 2] = true;
+            boolArray[9, 4] = true;
+            boolArray[9, 5] = true;
+            boolArray[9, 10] = true;
+            // Десятая строчка
+            boolArray[10, 9] = true;
+            boolArray[10, 10] = true;
+
+            //Границы лабиринта
+            for (int x = 0; x < 12; x++)
+            {
+                for (int y = 0; y < 12; y++)
+                {
+                    boolArray[0, y] = true; // левая граница
+                    boolArray[11, y] = true; // правая граница
+                    boolArray[x, 0] = true; // верхняя граница
+                    boolArray[x, 11] = true; // нижняя граница
+                }
+            }
+
+            // Создание графики лабиринта
+            stringArray = CreateGraphicalMap(boolArray, "#", "0");
+
+            DrawMap(stringArray);
+            Console.ReadKey();
+            Console.WriteLine();
+
+
+            while (!(coordX == 1 && coordY == 10))
+            {
+                Move(ref coordX, ref coordY, stringArray, boolArray);
+                steps++;
+            }
+
+            Console.WriteLine($"\nПрограмма завершилась успешно!");
+            Console.ReadKey();
+        }
+
+        // Движение
+        static void Move(ref int x, ref int y, string[,] graphicalMap, bool[,] logicalMap)
+        {
+            if (CanMove(x - 1, y, logicalMap))
+            {
+                x--; // Движение вверх
+                logicalMap[x, y] = true; // Метка о пройденном пути
+                graphicalMap[x, y] = "X"; // Графика перемещения
+                DrawMap(graphicalMap); // Отрисовка карты
+            }
+            else if (CanMove(x, y + 1, logicalMap))
+            {
+                y++; // Движение вправо
+                logicalMap[x, y] = true;
+                graphicalMap[x, y] = "X";
+                DrawMap(graphicalMap);
+            }
+            else if (CanMove((x + 1), y, logicalMap))
+            {
+                x++; // Движение вниз
+                logicalMap[x, y] = true;
+                graphicalMap[x, y] = "X";
+                DrawMap(graphicalMap);
+            }
+            else if (CanMove(x, y - 1, logicalMap))
+            {
+                y--; // Движение влево
+                logicalMap[x, y] = true;
+                graphicalMap[x, y] = "X";
+                DrawMap(graphicalMap);
+            }
+        }
+
+        // Проверка возможности перемещения
+        static bool CanMove(int x, int y, bool[,] logicalMap)
+        {
+            return !logicalMap[x, y];
+        }
+
+        // Отрисовка карты
+        static void DrawMap(string[,] graphicalMap)
+        {
+            Thread.Sleep(200);
+            Console.Clear();
+
+            for (int x = 1; x < 11; x++)
+            {
+                Console.WriteLine();
+                for (int y = 1; y < 11; y++)
+                {
+                    Console.Write(graphicalMap[x, y]);
+                }
+            }
+        }
+
+        // Генерация графики для логической карты
+        static string[,] CreateGraphicalMap(bool[,] logicMap, string wallSymbol = "#",
+                                        string emptySymbol = "0", int startX = 10, int startY = 1)
+        {
+            string[,] graphicalMap = new string[12, 12];
+
+            for (int x = 0; x < 11; x++)
+            {
+                for (int y = 0; y < 11; y++)
+                {
+                    if (logicMap[x, y])
+                    {
+                        graphicalMap[x, y] = wallSymbol;
+                    }
+                    else
+                    {
+                        graphicalMap[x, y] = emptySymbol;
+                    }
+                }
+            }
+            graphicalMap[startX, startY] = "X"; //Start point 
+            return graphicalMap;
+        }
+    }
+
+}
---
+++ LabAutomatsOOP/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Threading;
+
+namespace LabAutomatsOOP
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Map map1 = new Map(10, 10);
+
+            // Препятствия лабиринта
+            // Первая строчка
+            map1.AddWall(1, 8);
+            // Вторая строчка
+            map1.AddWall(2, 2);
+            map1.AddWall(2, 8);
+            // Третья строчка
+            map1.AddWall(3, 2);
+            map1.AddWall(3, 3);
+            map1.AddWall(3, 5);
+            map1.AddWall(3, 6);
+            // Четвертая строчка
+            map1.AddWall(4, 3);
+            map1.AddWall(4, 10);
+            // Пятая строчка
+            map1.AddWall(5, 6);
+            map1.AddWall(5, 7);
+            map1.AddWall(5, 8);
+            map1.AddWall(5, 9);
+            map1.AddWall(5, 10);
+            // Шестая строчка
+            map1.AddWall(6, 2);
+            // Седьмая строчка
+            map1.AddWall(7, 5);
+            map1.AddWall(7, 7);
+            map1.AddWall(7, 8);
+            // Восьмая строчка
+            map1.AddWall(8, 4);
+            map1.AddWall(8, 5);
+            map1.AddWall(8, 7);
+            // Девятая строчка
+            map1.AddWall(9, 1);
+            map1.AddWall(9, 2);
+            map1.AddWall(9, 4);
+            map1.AddWall(9, 5);
+            map1.AddWall(9, 10);
+            // Десятая строчка
+            map1.AddWall(10, 9);
+            map1.AddWall(10, 10);
+
+            Console.WriteLine("Выберите игру. \nДоступные варианты: \n1. Автопрохождение лабиринта\n2. Прохождение игроком");
+            string gameChooser = Console.ReadLine();
+
+            switch (gameChooser)
+            {
+                case "1":
+                    Console.WriteLine("Автоматическое прохождение. Нажмите кнопку...");
+                    map1.Draw();
+                    Console.ReadKey();
+                    map1.Game1(10, 1, 1, 10);
+                    Console.ReadKey();
+                    break;
+                case "2":
+                    Console.WriteLine("Прохождение игроком. Нажмите кнопку...");
+                    map1.Draw();
+                    Console.ReadKey();
+                    map1.Game2(10, 1, 1, 10);
+                    Console.ReadKey();
+                    break;
+                case "":
+                    Console.WriteLine("Введите еще раз");
+                    break;
+            }
+        }
+    }
+
+    internal class Map
+    {
+        private readonly string[,] _stringArray;
+        private int _coordX;
+        private int _coordY;
+        private int _steps = 1;
+        private char _pressedKey;
+
+        /// <summary>
+        /// Конструктор лабиринта по параметрам размера X и Y
+        /// </summary>
+        /// <param name="sizeX"></param>
+        /// <param name="sizeY"></param>
+        public Map(int sizeX, int sizeY)
+        {
+            _stringArray = new string[sizeX + 2, sizeY + 2];
+            
+            // Создание рамки вокруг лабиринта
+            for (int x = 0; x < _stringArray.GetLength(0); x++)
+            {
+                for (int y = 0; y < _stringArray.GetLength(1); y++)
+                {
+                    _stringArray[x, y] = "0";
+                    _stringArray[x, 0] = "|"; // Левая граница
+                    _stringArray[x, _stringArray.GetLength(1) - 1] = "|"; // Правая граница
+                    _stringArray[0, y] = "—"; // Верхняя граница
+                    _stringArray[_stringArray.GetLength(0) - 1, y] = "—"; // Нижняя граница
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод для добавления стен
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void AddWall(int x, int y)
+        {
+            if (x < _stringArray.GetLength(0) && y < _stringArray.GetLength(1))
+            {
+                _stringArray[x, y] = "#";
+            }
+        }
+        
+        /// <summary>
+        /// Отрисовка карты с таймером (сделать более постоянный таймер)
+        /// </summary>
+        public void Draw()
+        {
+            Thread.Sleep(200);
+            Console.Clear();
+            Console.WriteLine("Карта лабиринта");
+            Console.WriteLine($"Шагов сделано: {_steps}");
+            for (int x = 0; x < _stringArray.GetLength(0); x++)
+            {
+                Console.WriteLine();
+                for (int y = 0; y < _stringArray.GetLength(1); y++)
+                {
+                    Console.Write(_stringArray[x, y]);
+                }
+            }
+
+            Console.Write("\nВвод: ");
+        }
+        
+        /// <summary>
+        /// Игра с автоматическим поиском пути от startX и StartY
+        /// К точке finishX и FinishY
+        /// </summary>
+        /// <param name="startX"></param>
+        /// <param name="startY"></param>
+        /// <param name="finishX"></param>
+        /// <param name="finishY"></param>
+        public void Game1(int startX, int startY, int finishX, int finishY)
+        {
+            _coordX = startX;
+            _coordY = startY;
+            _stringArray[_coordX, _coordY] = "X";
+
+            bool CanMove(int x, int y)
+            {
+                return (_stringArray[x, y] == "0");
+            }
+
+            Console.WriteLine("\nИгра началась!");
+            while (!(_coordX == finishX & _coordY == finishY))
+            { 
+                if (CanMove(_coordX - 1, _coordY))
+                {
+                    _coordX--; // Движение вверх
+                    _stringArray[_coordX, _coordY] = "X"; // Графика перемещения
+                    Draw(); // Отрисовка карты
+                }
+                else if (CanMove(_coordX, _coordY + 1))
+                {
+                    _coordY++; // Движение вправо
+                    _stringArray[_coordX, _coordY] = "X";
+                    Draw();
+                }
+                else if (CanMove(_coordX + 1, _coordY))
+                {
+                    _coordX++; // Движение вниз
+                    _stringArray[_coordX, _coordY] = "X";
+                    Draw();
+                }
+                else if (CanMove(_coordX, _coordY - 1))
+                {
+                    _coordY--; // Движение влево
+                    _stringArray[_coordX, _coordY] = "X";
+                    Draw();
+                }
+
+                _steps++;
+            }
+
+            Console.WriteLine("\nУровень пройден!");
+        }
+
+        /// <summary>
+        /// Игра с управлением и поиском выхода
+        /// </summary>
+        /// <param name="startX"></param>
+        /// <param name="startY"></param>
+        /// <param name="finishX"></param>
+        /// <param name="finishY"></param>
+        public void Game2(int startX, int startY, int finishX, int finishY)
+        {
+            _coordX = startX;
+            _coordY = startY;
+            _stringArray[_coordX, _coordY] = "X";
+
+            bool CanMove(int x, int y)
+            {
+                return (_stringArray[x, y] == "0");
+            }
+
+            Console.WriteLine("\nИгра началась!");
+            while (!(_coordX == finishX & _coordY == finishY))
+            {
+                
+                _pressedKey = Console.ReadKey().KeyChar;
+                
+                if ((_pressedKey == 'w' || _pressedKey == 'W') && CanMove(_coordX - 1, _coordY))
+                {
+                    _coordX--; // Движение вверх
+                    _stringArray[_coordX, _coordY] = "X"; // Графика перемещения
+                    Draw(); // Отрисовка карты
+                    _steps++;
+                }
+                else if ((_pressedKey == 'd' || _pressedKey == 'D') && CanMove(_coordX, _coordY + 1))
+                {
+                    _coordY++; // Движение вправо
+                    _stringArray[_coordX, _coordY] = "X";
+                    Draw();
+                    _steps++;
+                }
+                else if ((_pressedKey == 's' || _pressedKey == 'S') && CanMove(_coordX + 1, _coordY))
+                {
+                    _coordX++; // Движение вниз
+                    _stringArray[_coordX, _coordY] = "X";
+                    Draw();
+                    _steps++;
+                }
+                else if ((_pressedKey == 'a' || _pressedKey == 'A') && CanMove(_coordX, _coordY - 1))
+                {
+                    _coordY--; // Движение влево
+                    _stringArray[_coordX, _coordY] = "X";
+                    Draw();
+                    _steps++;
+                }
+            }
+
+            Console.WriteLine("\nУровень пройден!");
+        }
+    }
+}
